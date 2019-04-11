ed8e903c6dce209ef78cc406f6c3e41c1b9fe301
Divakar Poosarla <divakarpoosarla123@gmail.com>
2019-04-11 14:16:28 +0530

    small errro fixed

--- client/src/components/User/ChangePassword.js
+++ client/src/components/User/ChangePassword.js
@@ -33,31 +33,34 @@ class ChangePassword extends Component{
         }
     }
     render(){
+        let errblk=(this.state.err!="")?(<div className="alert alert-danger" ><strong>Err : </strong>{this.state.err}</div>):(" ");
         return(
-            <div style={{marginRight:"50px",marginLeft:"50px"}}>
-                <h1 id="changepassword">Change Password</h1>
-                <form  onSubmit={this.handleSubmit}>
-                    <div className="form-group" id="prof1">
-                        <label htmlFor="Currpassword" >Current Password</label>
-                        <input type="password" className="form-control" name="Currpassword" aria-describedby="emailHelp" placeholder="Enter current password" />
-                    </div>
+            <div >
+                <div style={{marginRight:"50px",marginLeft:"50px",marginBottom:"10px"}}>
+                    <h1 id="changepassword">Change Password</h1>
+                    <form  onSubmit={this.handleSubmit}>
+                        <div className="form-group" id="prof1">
+                            <label htmlFor="Currpassword" >Current Password</label>
+                            <input type="password" className="form-control" name="Currpassword" aria-describedby="emailHelp" placeholder="Enter current password" />
+                        </div>
 
-                    <div className="form-group" id="prof1">
-                        <label htmlFor="Newpassword" >New Password</label>
-                        <input type="password" className="form-control" name="Newpassword" aria-describedby="emailHelp" placeholder="Enter New password" />
-                    </div>
+                        <div className="form-group" id="prof1">
+                            <label htmlFor="Newpassword" >New Password</label>
+                            <input type="password" className="form-control" name="Newpassword" aria-describedby="emailHelp" placeholder="Enter New password" />
+                        </div>
 
-                    <div className="form-group" id="prof1">
-                        <label htmlFor="Confpassword" >Confirm Password</label>
-                        <input type="password" className="form-control" name="ConfPassword" aria-describedby="emailHelp" placeholder="Cofirm password" />
-                    </div>
+                        <div className="form-group" id="prof1">
+                            <label htmlFor="Confpassword" >Confirm Password</label>
+                            <input type="password" className="form-control" name="ConfPassword" aria-describedby="emailHelp" placeholder="Cofirm password" />
+                        </div>
 
-                    <div className="container-button" id="prof">
-                        <button type="submit" className="btn btn-primary" id="register-submit">Change</button>
-                    </div>
-                </form>
-                <div className="error" className="alert alert-danger" >
-                    <strong>Err:{this.state.err}</strong>
+                        <div className="container-button" id="prof">
+                            <button type="submit" className="btn btn-primary" id="register-submit">Change</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="error" >
+                    {errblk}
                 </div>
             </div>
         )
